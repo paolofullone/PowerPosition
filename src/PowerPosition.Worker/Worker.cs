211734b4c4ca7d19f946887e07b503bab954535b
paolo.fullone <paolo.enrico@gmail.com>
2025-05-13 06:59:45 -0300

    use power constants, log utc and london time on ExecuteAsync

--- src/PowerPosition.Worker/Worker.cs
+++ src/PowerPosition.Worker/Worker.cs
@@ -1,3 +1,4 @@
+using PowerPosition.Worker.Constants;
 using PowerPosition.Worker.Services;
 
 namespace PowerPosition.Worker
@@ -7,16 +8,27 @@ namespace PowerPosition.Worker
         private readonly IPowerPositionService _service;
         private readonly ILogger<Worker> _logger;
         private readonly TimeSpan _interval;
+        private readonly TimeZoneInfo _londonTimeZone;
+        private readonly DateTime _utcNow;
+        private readonly DateTime _londonNow;
 
         public Worker(IPowerPositionService service, ILogger<Worker> logger, IConfiguration configuration)
         {
             _service = service;
             _logger = logger;
-            _interval = TimeSpan.FromMinutes(configuration.GetValue<int>("PowerPosition:IntervalInMinutes"));
+            _londonTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Europe/London");
+            _utcNow = DateTime.UtcNow;
+            _londonNow = TimeZoneInfo.ConvertTimeFromUtc(_utcNow, _londonTimeZone);
+            _interval = TimeSpan.FromMinutes(configuration.GetValue<int>(PowerPositionConstants.settingsInterval));
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            _logger.LogInformation(
+                "Using interval: {IntervalMinutes} minute(s) at Utc: {UtcTime} (Local: {LocalTime})",
+                _interval.TotalMinutes, _utcNow, _londonNow
+                );
+
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Power Worker running with interval of {interval} minute(s)", _interval.TotalMinutes);
