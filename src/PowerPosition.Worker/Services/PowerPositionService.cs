ae9c88d909c69a45d9bf0a47a4f413e97a711b19
paolo.fullone <paolo.enrico@gmail.com>
2025-05-13 07:15:18 -0300

    test periodic executions

--- src/PowerPosition.Worker/Services/PowerPositionService.cs
+++ src/PowerPosition.Worker/Services/PowerPositionService.cs
@@ -7,16 +7,23 @@ public class PowerPositionService : IPowerPositionService
     private readonly IPowerService _powerService;
     private readonly ILogger<PowerPositionService> _logger;
     private readonly string _outputFolder;
+    private readonly TimeZoneInfo _londonTimeZone;
 
     public PowerPositionService(IPowerService powerService, ILogger<PowerPositionService> logger, string outputFolder)
     {
         _powerService = powerService;
         _logger = logger;
         _outputFolder = outputFolder;
+        _londonTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Europe/London");
     }
 
     public async Task GenerateReportAsync(CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var utcNow = DateTime.UtcNow;
+        var londonNow = TimeZoneInfo.ConvertTimeFromUtc(utcNow, _londonTimeZone);
+
+        _logger.LogInformation(
+            "Generate report async at Utc: {UtcTime} (Europe/London: {LocalTime}",
+            utcNow, londonNow);
     }
 }
