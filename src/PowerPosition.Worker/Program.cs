43a02a596cd89596e6229dbcc06dae04ac7da4f9
paolo.fullone <paolo.enrico@gmail.com>
2025-05-13 06:42:54 -0300

    add interval from appsettings replace hardcoded by constants

--- src/PowerPosition.Worker/Program.cs
+++ src/PowerPosition.Worker/Program.cs
@@ -1,18 +1,20 @@
 using Axpo;
 using PowerPosition.Worker;
+using PowerPosition.Worker.Constants;
 using PowerPosition.Worker.Services;
 
 var builder = Host.CreateApplicationBuilder(args);
+
 builder.Services.AddHostedService<Worker>();
 
-var outputFolder = builder.Configuration.GetValue<string>("PowerPosition:OutputFolder");
+var interval = TimeSpan.FromMinutes(builder.Configuration.GetValue<int>(Constants.settingsInterval, Constants.defaultInterval));
+
+var outputFolder = builder.Configuration.GetValue<string>(Constants.settingsOutputFolder);
 
 builder.Services.AddSingleton<IPowerPositionService, PowerPositionService>();
 builder.Services.AddSingleton<IPowerService, PowerService>();
 
-builder.Services.AddSingleton(sp =>
-    builder.Configuration.GetValue<string>("PowerPosition:OutputFolder"));
-
+builder.Services.AddSingleton(sp => outputFolder);
 
 var host = builder.Build();
 host.Run();
--- src/PowerPosition.Worker/Worker.cs
+++ src/PowerPosition.Worker/Worker.cs
@@ -6,17 +6,20 @@ namespace PowerPosition.Worker
     {
         private readonly IPowerPositionService _service;
         private readonly ILogger<Worker> _logger;
+        private readonly TimeSpan _interval;
 
-        public Worker(IPowerPositionService service, ILogger<Worker> logger)
+        public Worker(IPowerPositionService service, ILogger<Worker> logger, IConfiguration configuration)
         {
             _service = service;
             _logger = logger;
+            _interval = TimeSpan.FromMinutes(configuration.GetValue<int>("PowerPosition:IntervalInMinutes"));
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
+                _logger.LogInformation("Power Worker running with interval of {interval} minute(s)", _interval.TotalMinutes);
                 await _service.GenerateReportAsync(stoppingToken);
             }
         }
