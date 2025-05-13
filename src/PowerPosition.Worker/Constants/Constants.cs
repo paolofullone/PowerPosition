74a5a7223149744e3a25494bb0005f1583b90c03
paolo.fullone <paolo.enrico@gmail.com>
2025-05-13 06:41:21 -0300

    add constants

---
+++ src/PowerPosition.Worker/Constants/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace PowerPosition.Worker.Constants
+{
+    public static class Constants
+    {
+        public const string settingsInterval = "PowerPosition:IntervalInMinutes";
+        public const int defaultInterval = 60;
+        public const string settingsOutputFolder = "PowerPosition:OutputFolder";
+    }
+}
