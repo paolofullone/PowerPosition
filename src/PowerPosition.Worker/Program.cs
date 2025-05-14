ff6e7aa9a1692236861277b220db88f0e1e2b58b
paolo.fullone <paolo.enrico@gmail.com>
2025-05-14 08:00:45 -0300

    improve DI

--- src/PowerPosition.Worker/Program.cs
+++ src/PowerPosition.Worker/Program.cs
@@ -1,18 +1,17 @@
 using Axpo;
 using PowerPosition.Worker;
-using PowerPosition.Worker.Constants;
+using PowerPosition.Worker.Configuration;
 using PowerPosition.Worker.Services;
 
 var builder = Host.CreateApplicationBuilder(args);
 
 builder.Services.AddHostedService<Worker>();
 
-var outputFolder = builder.Configuration.GetValue<string>(PowerPositionConstants.settingsOutputFolder);
+builder.Services.Configure<PowerPositionSettings>(builder.Configuration.GetSection("PowerPositionSettings"));
 
 builder.Services.AddSingleton<IPowerPositionService, PowerPositionService>();
 builder.Services.AddSingleton<IPowerService, PowerService>();
 
-builder.Services.AddSingleton(sp => outputFolder);
 
 var host = builder.Build();
 host.Run();
