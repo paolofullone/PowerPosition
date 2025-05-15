3d134f7014557c9ea911f40b84eb318a56f56381
paolo.fullone <paolo.enrico@gmail.com>
2025-05-14 21:56:10 -0300

    add serilog

--- src/PowerPosition.Worker/Program.cs
+++ src/PowerPosition.Worker/Program.cs
@@ -2,8 +2,20 @@ using Axpo;
 using PowerPosition.Worker;
 using PowerPosition.Worker.Configuration;
 using PowerPosition.Worker.Services;
+using Serilog;
+using Serilog.Formatting.Compact;
+
+var logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Console(new CompactJsonFormatter())
+    .CreateLogger();
+
+Log.Logger = logger;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
 
 builder.Services.AddHostedService<Worker>();
 
@@ -11,7 +23,6 @@ builder.Services.Configure<PowerPositionSettings>(builder.Configuration.GetSecti
 
 builder.Services.AddSingleton<IPowerPositionService, PowerPositionService>();
 builder.Services.AddSingleton<IPowerService, PowerService>();
-
 
 var host = builder.Build();
 host.Run();
