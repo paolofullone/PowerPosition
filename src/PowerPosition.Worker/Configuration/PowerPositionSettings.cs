ee78e9951d3afa5d9605fec898b0d6c83af48ca2
paolo.fullone <paolo.enrico@gmail.com>
2025-06-15 16:31:18 -0300

    update settings

--- src/PowerPosition.Worker/Configuration/PowerPositionSettings.cs
+++ src/PowerPosition.Worker/Configuration/PowerPositionSettings.cs
@@ -4,8 +4,9 @@ namespace PowerPosition.Worker.Configuration;
 
 public class PowerPositionSettings
 {
-    public string OutputFolder { get; set; } = string.Empty;
-    public string LocalTimeZone { get; set; } = string.Empty;
-    public int IntervalInMinutes { get; set; } = PowerPositionConstants.defaultIntervalInMinutes;
-    public int RetryDelayMilliseconds { get; set; } = PowerPositionConstants.defaultDelayMiliseconds;
+    public string OutputFolder { get; set; } = PowerPositionConstants.OutputFolder;
+    public int IntervalInMinutes { get; set; } = PowerPositionConstants.IntervalInMinutes;
+    public string LocalTimeZone { get; set; } = PowerPositionConstants.localTimeZone;
+    public int InitialRetryDelayMilliseconds { get; set; } = PowerPositionConstants.MaxRetryAttempts;
+    public int MaxRetryAttempts { get; set; } = PowerPositionConstants.MaxRetryAttempts;
 }
--- src/PowerPosition.Worker/Constants/PowerPositionConstants.cs
+++ src/PowerPosition.Worker/Constants/PowerPositionConstants.cs
@@ -2,9 +2,26 @@
 {
     public class PowerPositionConstants
     {
-        public const string settingsInterval = "PowerPosition:IntervalInMinutes";
-        public const int defaultIntervalInMinutes = 60;
-        public const string settingsOutputFolder = "PowerPosition:OutputFolder";
-        public const int defaultDelayMiliseconds = 500;
+        #region GeneralSettings
+        public const string settingsInterval = "PowerPositionSettings:IntervalInMinutes";
+        public const int IntervalInMinutes = 1;
+        public const string settingsOutputFolder = "PowerPositionSettings:OutputFolder";
+        public const string OutputFolder = "../../reports";
+        public const string settingsLocalTimeZone = "PowerPositionSettings:LocalTimeZone";
+        public const string localTimeZone = "Europe/London";
+        public const string settingsInitialRetry = "PowerPositionSettings:InitialRetryDelayMilliseconds";
+        public const int InitialRetryDelayMilliseconds = 100;
+        public const string settingsMaxRetryAttempts = "PowerPositionSettings:MaxRetryAttempts";
+        public const int MaxRetryAttempts = 5;
+        #endregion
+
+
+        #region VolumeCalculator
+        public const int HoursInDay = 24;
+        public const int FirstPeriod = 1;
+        public const int LastPeriod = 24;
+        public const int MinArrayIndex = 0;
+        public const int MaxArrayIndex = HoursInDay - 1;
+        #endregion
     }
 }
